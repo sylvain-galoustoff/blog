cc3f33a92c884d9ad6e01d9a9e6024ffbdb524b6
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-02-08 00:05:55 +0100

    static home ok

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,7 +11,21 @@ function Home() {
     }, 2000);
   });
 
-  return <div className="page" id="home"></div>;
+  return (
+    <div className="page" id="home">
+      <div id="title-wrapper">
+        <h1>Sylvain Galoustoff</h1>
+        <div id="site-title">
+          <div id="ui">UI</div>
+          <div id="definitions">
+            <p>Developper</p>
+            <p>Designer</p>
+            <p>Lover</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
